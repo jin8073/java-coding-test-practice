34a327e55709d0440b38b506d53a17fc5fccdbd9
jin8073 <jin8073@gmail.com>
2023-03-15 14:39:30 +0900

    프로그래머스 코딩테스트 연습문제 Lv.0 - 나머지 구하기
    - 두 수의 크기 비교

--- src/main/java/programmers/lv0/Problem010.java
+++ src/main/java/programmers/lv0/Problem010.java
@@ -5,7 +5,7 @@ package programmers.lv0;
  * */
 public class Problem010 {
     public int solution(int num1, int num2) {
-        boolean validation = (num1 > 0 && num1 <= 100) &&  (num2 > 0 && num2 <= 100);
+        boolean validation = (num1 > 0 && num1 <= 100) &&  (num2 > 0 && num2 <= 100) && (num1 > num2);
         int answer = -1;
         if (validation) {
             answer = num1 % num2;
