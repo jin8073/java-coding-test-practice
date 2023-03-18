af5b6b58eb7343c551d34a99061a697b3513b5bb
jin8073 <jin8073@gmail.com>
2023-03-18 19:19:00 +0900

    프로그래머스 코딩테스트 연습문제 Lv.0 - 나이 출력
    - 문제 풀이

---
+++ src/main/java/programmers/lv0/Problem012.java
@@ -0,0 +1,22 @@
+package programmers.lv0;
+
+/** 나이 출력
+ * 나이 age가 주어짐(0 < age <= 120)
+ * 2022년도 기준 출생 연도를 return
+ * */
+public class Problem012 {
+    public int solution(int age) {
+        int answer = 0;
+        if (checkValidation(age)) {
+            answer = 2022 - age + 1;
+        }
+        return answer;
+    }
+
+    private boolean checkValidation(int age) {
+        if (age > 0 && age <= 120) {
+            return true;
+        }
+        return false;
+    }
+}
