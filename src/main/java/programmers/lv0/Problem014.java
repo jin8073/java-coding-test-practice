9f5545aca1a2b0762dc6189455930d591d698e96
jin8073 <jin8073@gmail.com>
2023-03-19 18:58:44 +0900

    프로그래머스 코딩테스트 연습문제 Lv.0 - 짝수의 합
    - 문제 풀이

---
+++ src/main/java/programmers/lv0/Problem014.java
@@ -0,0 +1,32 @@
+package programmers.lv0;
+
+/** 짝수의 합
+ * 매개변수로 정수 n이 주어짐(0 < n <= 1000)
+ * n 이하의 짝수를 모두 더한 값을 return
+ * */
+public class Problem014 {
+    public int solution(int n) {
+        int answer = 0;
+        if (checkValidation(n)) {
+            answer = getAnswer(n);
+        }
+        return answer;
+    }
+
+    private boolean checkValidation(int n) {
+        if (n > 0 && n <= 1000) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getAnswer(int n) {
+        int answer = 0;
+        if (n > 1) {
+            for (int i = 2; i <= n; i+=2) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
