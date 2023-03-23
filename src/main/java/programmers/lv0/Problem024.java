ec2be1664ffb427232c563c851d8763a32a65176
jin8073 <jin8073@gmail.com>
2023-03-23 18:06:21 +0900

    프로그래머스 코딩테스트 연습문제 Lv.0 - 최댓값 만들기 (1)

---
+++ src/main/java/programmers/lv0/Problem024.java
@@ -0,0 +1,26 @@
+package programmers.lv0;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 최댓값 만들기 (1)
+ * */
+public class Problem024 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int first = 0;
+        int second = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= first) {
+                second = first;
+                first = numbers[i];
+            } else if (numbers[i] >= second && numbers[i] < first) {
+                second = numbers[i];
+            }
+        }
+        answer = first * second;
+        return answer;
+    }
+}
