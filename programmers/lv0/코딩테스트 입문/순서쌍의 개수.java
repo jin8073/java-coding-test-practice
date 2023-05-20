caa82d14a25f1d192f500fdca2f7ffc44fa0a7d4
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-05-20 21:00:03 +0900

    Update and rename src/main/java/programmers/lv0/Problem018.java to programmers/lv0/코딩테스트 입문/순서쌍의 개수.java

--- programmers/lv0/코딩테스트 입문/순서쌍의 개수.java
+++ programmers/lv0/코딩테스트 입문/순서쌍의 개수.java
@@ -1,9 +1,7 @@
-package programmers.lv0;
-
 /**
  * 순서쌍의 개수
  * */
-public class Problem018 {
+class Solution {
     public int solution(int n) {
         int answer = 0;
         for (int i = 1; i <= n; i++) {
