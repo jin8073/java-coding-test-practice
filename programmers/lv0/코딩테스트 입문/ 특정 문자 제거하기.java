11038fbfebdd4b30d4a0c7e21a3cd2f5abdb4747
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-05-20 21:07:28 +0900

    Update and rename src/main/java/programmers/lv0/Problem029.java to programmers/lv0/코딩테스트 입문/ 특정 문자 제거하기.java

--- programmers/lv0/코딩테스트 입문/ 특정 문자 제거하기.java
+++ programmers/lv0/코딩테스트 입문/ 특정 문자 제거하기.java
@@ -1,9 +1,7 @@
-package programmers.lv0;
-
 /**
  * 특정 문자 제거하기
  * */
-public class Problem029 {
+class Solution {
     public String solution(String my_string, String letter) {
         String answer = "";
         for (char my_char : my_string.toCharArray()) {
