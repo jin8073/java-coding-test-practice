a414d31c11770cf863bc0a40639a424241b8862c
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-05-20 20:56:20 +0900

    Update 머쓱이보다 키 큰 사람.java

--- programmers/lv0/코딩테스트 입문/머쓱이보다 키 큰 사람.java
+++ programmers/lv0/코딩테스트 입문/머쓱이보다 키 큰 사람.java
@@ -1,9 +1,7 @@
-package programmers.lv0;
-
 /**
  * 머쓱이보다 키 큰 사람
  * */
-public class Problem002 {
+class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
         for (int heightOfClassmate : array) {
