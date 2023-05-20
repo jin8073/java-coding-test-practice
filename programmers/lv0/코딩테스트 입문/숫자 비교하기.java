e7dc3ff3d5d01ec419824abc05cdf42e4e5362fd
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-05-20 20:57:39 +0900

    Update 숫자 비교하기.java

--- programmers/lv0/코딩테스트 입문/숫자 비교하기.java
+++ programmers/lv0/코딩테스트 입문/숫자 비교하기.java
@@ -1,9 +1,7 @@
-package programmers.lv0;
-
 /**
  * 숫자 비교하기
  * */
-public class Problem008 {
+class Solution {
     public int solution(int num1, int num2) {
         boolean validation = (num1 >= 0 && num1 <= 10000) && (num2 >= 0 && num2 <= 10000);
         int answer = 0;
