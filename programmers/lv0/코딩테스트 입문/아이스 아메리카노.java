2d4983ef53179a6e29a73fbd22c91bed1c541ea8
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-05-20 21:06:18 +0900

    Update and rename src/main/java/programmers/lv0/Problem027.java to programmers/lv0/코딩테스트 입문/아이스 아메리카노.java

--- programmers/lv0/코딩테스트 입문/아이스 아메리카노.java
+++ programmers/lv0/코딩테스트 입문/아이스 아메리카노.java
@@ -1,9 +1,7 @@
-package programmers.lv0;
-
 /**
  * 아이스 아메리카노
  * */
-public class Problem027 {
+class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
         answer[0] = money / 5500;
