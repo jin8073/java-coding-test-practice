0a1822656302d8e41238c6217feba81ee5f3dc6b
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-05-20 20:55:15 +0900

    Update 두 수의 나눗셈.java

--- programmers/lv0/코딩테스트 입문/두 수의 나눗셈.java
+++ programmers/lv0/코딩테스트 입문/두 수의 나눗셈.java
@@ -1,9 +1,7 @@
-package programmers.lv0;
-
 /**
  * 두 수의 나눗셈
  * */
-public class Problem007 {
+class Solution {
     public int solution(int num1, int num2) {
         boolean validation = (num1 > 0 && num1 <= 100) && (num2 > 0 && num2 <= 100);
         int answer = -1;
