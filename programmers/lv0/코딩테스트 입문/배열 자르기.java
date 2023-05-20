4118df5aca5d80e897bfbb26266baa5c00e2c871
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-05-20 21:06:50 +0900

    Update and rename src/main/java/programmers/lv0/Problem028.java to programmers/lv0/코딩테스트 입문/배열 자르기.java

--- programmers/lv0/코딩테스트 입문/배열 자르기.java
+++ programmers/lv0/코딩테스트 입문/배열 자르기.java
@@ -1,9 +1,7 @@
-package programmers.lv0;
-
 /**
  * 배열 자르기
  * */
-public class Problem028 {
+class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
         int[] answer = new int[num2 - num1 + 1];
         for (int i = num1; i <= num2; i++) {
