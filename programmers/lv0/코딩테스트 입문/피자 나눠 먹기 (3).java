8c8989a68acd9849c496ec1c89a041cdf4200ce4
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-05-20 21:01:15 +0900

    Update and rename src/main/java/programmers/lv0/Problem020.java to programmers/lv0/코딩테스트 입문/피자 나눠 먹기 (3).java

--- programmers/lv0/코딩테스트 입문/피자 나눠 먹기 (3).java
+++ programmers/lv0/코딩테스트 입문/피자 나눠 먹기 (3).java
@@ -1,9 +1,7 @@
-package programmers.lv0;
-
 /**
  * 피자 나눠 먹기 (3)
  * */
-public class Problem020 {
+class Solution {
     public int solution(int slice, int n) {
         int answer = 0;
         answer = n / slice;
