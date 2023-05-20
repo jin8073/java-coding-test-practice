86cbd367c87ffe9ddad5fa108d205f66f1e0be22
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-05-20 20:57:10 +0900

    Update 배열의 평균값.java

--- programmers/lv0/코딩테스트 입문/배열의 평균값.java
+++ programmers/lv0/코딩테스트 입문/배열의 평균값.java
@@ -1,12 +1,10 @@
-package programmers.lv0;
-
 /** 배열의 평균값
  * 정수 배열 numbers가 매개변수로 주어짐
  * 0 <= numbers의 원소 <= 1000, 1 <= numbers의 길이 <= 100
  * numbers의 원소의 평균값을 return
  * 정답의 소수 부분이 .0 또는 .5인 경우만 입력으로 주어짐
  * */
-public class Problem015 {
+class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
         if (checkValidation(numbers)) {
