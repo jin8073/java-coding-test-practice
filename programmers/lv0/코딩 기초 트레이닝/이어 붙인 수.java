182f8d1003520e1034490bb4fe17f41d66291119
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-08-02 16:17:08 +0900

    Create 이어 붙인 수.java

---
+++ programmers/lv0/코딩 기초 트레이닝/이어 붙인 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String odd = "";
+        String even = "";
+        for (int num : num_list) {
+            if (num % 2 == 0) {
+                even += num;
+            } else {
+                odd += num;
+            }
+        }
+        answer = Integer.parseInt(odd) + Integer.parseInt(even);
+        return answer;
+    }
+}
