3d6047a7984a55683732df4473889d9ef8bd6632
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-08-04 21:07:59 +0900

    Create 카운트 업.java

---
+++ programmers/lv0/코딩 기초 트레이닝/카운트 업.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[end-start+1];
+        int count = 0;
+        for (int i=start; i<=end; i++) {
+            answer[count] = i;
+            count++;
+        }
+        return answer;
+    }
+}
