51dadcd0e056896cd2f3f230d656326d48386c3b
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-08-04 21:05:18 +0900

    Update 배열 만들기 2.java

--- programmers/lv0/코딩 기초 트레이닝/배열 만들기 2.java
+++ programmers/lv0/코딩 기초 트레이닝/배열 만들기 2.java
@@ -1,1 +1,34 @@
-
+class Solution {
+    public int[] solution(int l, int r) {
+        int[] answer = {};
+        String answerStr = "";
+        while (l % 5 != 0) {
+            l++;
+        }
+        for (int i=l; i<=r; i+=5) {
+            int num = i;
+            while (num > 0) {
+                if (num % 5 != 0) {
+                    break;
+                }
+                num /= 10;
+            }
+            if (num == 0) {
+                if (answerStr.length() != 0) {
+                    answerStr += ",";
+                }
+                answerStr += i;
+            }
+        }
+        if (answerStr.length() != 0) {
+            String[] strArr = answerStr.split(",");
+            answer = new int[strArr.length];
+            for (int i=0; i<strArr.length; i++) {
+                answer[i] = Integer.parseInt(strArr[i]);
+            }
+        } else {
+            answer = new int[] {-1};
+        }
+        return answer;
+    }
+}
