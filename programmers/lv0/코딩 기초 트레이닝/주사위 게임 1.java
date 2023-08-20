f39d5f1fa517146c3e0758dd38d1c1669c9760bf
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-08-20 21:25:18 +0900

    Create 주사위 게임 1.java

---
+++ programmers/lv0/코딩 기초 트레이닝/주사위 게임 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if ((a * b) % 2 != 0) {
+            answer = a * a + b * b;
+        } else if ((a + b) % 2 == 0) {
+            answer = a > b ? a - b : b - a;
+        } else {
+            answer = 2 * (a + b);
+        }
+        return answer;
+    }
+}
