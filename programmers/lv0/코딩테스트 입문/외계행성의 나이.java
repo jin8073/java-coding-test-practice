774cb752019f3f1ad7cd877e01e6e78e2e008976
Jinkyoung_Heo <74819863+jin8073@users.noreply.github.com>
2023-09-20 22:31:34 +0900

    Create 외계행성의 나이.java

---
+++ programmers/lv0/코딩테스트 입문/외계행성의 나이.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        while (age > 0) {
+            answer = (char) (age % 10 + 97) + answer;
+            age /= 10;
+        }
+        return answer;
+    }
+}
